d946ee9105b044281334a2488529d88e1fab6cf6
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-02-27 22:41:00 +0000

    Add files via upload

---
+++ Laughter Yoga Website/script.js
@@ -0,0 +1,54 @@
+var l;
+var a;
+var u;
+var g;
+var h;
+var t;
+var e;
+var r;
+var y;
+var o;
+var g2;
+var a2;
+
+function init() {
+  l = document.getElementById("l");
+  a = document.getElementById("a");
+  u = document.getElementById("u");
+  g = document.getElementById("g");
+  h = document.getElementById("h");
+  t = document.getElementById("t");
+  e = document.getElementById("e");
+  r = document.getElementById("r");
+  y = document.getElementById("y");
+  o = document.getElementById("o");
+  g2 = document.getElementById("g2");
+  a2 = document.getElementById("a2");
+}
+
+// var audio = $("#sound")[0];
+// audio.play();
+
+// var beepOne = $("#sound");
+// $("h1").mouseenter(function() {
+// beepOne.play();
+// });
+
+// function removeTransition(e) {
+//   if (e.propertyName !== 'transform') return;
+//   e.target.classList.remove('playing');
+// }
+//
+// function playSound(e) {
+//   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//   if (!audio) return;
+//
+//   key.classList.add('playing');
+//   audio.currentTime = 0;
+//   audio.play();
+// }
+//
+// const keys = Array.from(document.querySelectorAll('.key'));
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// window.addEventListener('keydown', playSound);
