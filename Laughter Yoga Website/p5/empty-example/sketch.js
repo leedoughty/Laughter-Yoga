6896af59de56d9d1ffaf1aa86468e9c50b0796d0
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-03-16 11:40:02 +0000

    Added DOM interaction with the title
    Changes colour when mouse is moved

--- Laughter Yoga Website/p5/empty-example/sketch.js
+++ Laughter Yoga Website/p5/empty-example/sketch.js
@@ -1,20 +1,33 @@
 var canvas;
 var h1;
+// var value = 0;
 
 function setup() {
-  canvas = createCanvas(200, 200);
-  h1 = createElement("h1, test");
+  canvas = createCanvas(windowWidth, windowHeight);
+  h1 = select("#title");
+  // h1.mouseMoved(colorChange);
+  canvas.mouseMoved(colorChange);
 }
 
-function mouseMoved(){
-  value = value + 5;
-  if (value > 255) {
-  value = 0;
-  console.log("heyo");
-}
+function colorChange() {
+  h1.style("color", "#ff0000");
+  console.log("heyoo");
 }
 
-function draw() {
-  fill(value);
-  h1(25, 25, 50, 50);
-}
+// function draw() {
+//
+// }
+
+// function mouseMoved(){
+//   value = value + 5;
+//   if (value > 255) {
+//   value = 0;
+//   fill(value);
+//   console.log("heyo");
+// }
+// }
+
+// function draw() {
+//   fill(value);
+//   h1(25, 25, 50, 50);
+// }
